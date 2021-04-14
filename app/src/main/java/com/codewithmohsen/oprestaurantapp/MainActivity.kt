c49d7fa94e18f7c85589976c0a95718396d77392
Mohsen Sedighi <sedighi.66@gmail.com>
2021-04-15 02:19:59 +0430

    ui implemented, modified with the project design, dispatcher io implemented in repo.

--- app/src/main/java/com/codewithmohsen/oprestaurantapp/MainActivity.kt
+++ app/src/main/java/com/codewithmohsen/oprestaurantapp/MainActivity.kt
@@ -5,6 +5,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import dagger.android.AndroidInjection
 import dagger.android.AndroidInjector
@@ -26,9 +27,8 @@ class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-//        val appBarConfiguration = AppBarConfiguration(setOf(
-//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
-//        setupActionBarWithNavController(navController, appBarConfiguration)
+        val appBarConfiguration = AppBarConfiguration(setOf(
+                R.id.navigation_restaurants, R.id.navigation_dashboard, R.id.navigation_notifications))
         navView.setupWithNavController(navController)
     }
 
